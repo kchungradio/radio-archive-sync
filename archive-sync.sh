1cf1aa593f869c877aeff98d90ce428087f3fbbd
Tim Phillips <3578574+tim-phillips@users.noreply.github.com>
2024-06-06 10:36:48 -0700

    remove sync from studio computer script

--- archive-sync.sh
+++ archive-sync.sh
@@ -5,8 +5,7 @@
 #
 
 s3_log="/var/log/kchung/s3_log.txt"
-db_log="/var/log/kchung/db_log.txt"
-local_dir="/path/to/archive"
+local_dir="/Users/development/kchung/archive"
 remote_dir="s3://archive.kchungradio.org"
 
 #
@@ -19,15 +18,3 @@ printf "`date '+%Y-%m-%d  %H:%M:%S'`\n" >> $s3_log
 
 printf "`date '+%Y-%m-%d  %H:%M:%S'`\n" >> $s3_log
 printf "\n" >> $s3_log
-
-
-#
-# sync s3 to db
-#
-
-printf "`date '+%Y-%m-%d  %H:%M:%S'`\n" >> $db_log
-
-/usr/local/bin/pipenv run python sync.py >> $db_log 2>&1
-
-printf "`date '+%Y-%m-%d  %H:%M:%S'`\n" >> $db_log
-printf "\n" >> $db_log
